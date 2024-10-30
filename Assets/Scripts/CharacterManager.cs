cc0ec837c28eee136d63a17023de0169dfb4d405
Leesangmin <manhasum@naver.com>
2024-10-30 18:22:03 +0900

    0.1.7

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -156,20 +156,20 @@ public class CharacterManager : MonoBehaviour
         characterName[12] = "세레스";
         characterName[13] = "랑트";
 
-        characterDescription[0] = "서히로다";
-        characterDescription[1] = "이수혁이다";
-        characterDescription[2] = "타냐다";
-        characterDescription[3] = "카르멘이다";
-        characterDescription[4] = "이미주다";
-        characterDescription[5] = "표미르다";
-        characterDescription[6] = "곽다수다";
-        characterDescription[7] = "테오다";
-        characterDescription[8] = "필레몬이다";
-        characterDescription[9] = "클라우디아다";
-        characterDescription[10] = "노아다";
-        characterDescription[11] = "알타이르다";
-        characterDescription[12] = "세레스다";
-        characterDescription[13] = "랑트다";
+        characterDescription[0] = "달리기가 특기인 시간의 영웅. 하지만 지금은 도망치는 것 밖에 할 수 있는 것이 없다!";
+        characterDescription[1] = "사인참사검을 쓰는 청년 검객. 노란 옷의 왕은 이럴 때 도와주지 않고 뭐 하는 건지!";
+        characterDescription[2] = "공간의 힘을 다루는 맹인 성녀. 를뤼에의 주인조차 게임 속에선 도망칠 수 밖에 없다.";
+        characterDescription[3] = "네모나진 작고 사악한 과학자. 그녀를 지켜주던 동물 로봇들도 전부 사라져버렸다.";
+        characterDescription[4] = "고결한 이단심문관 성기사. 유니콘 고르디우스가 없어진 탓에 뚜벅이 신세가 되었다.";
+        characterDescription[5] = "세종시 참변의 유일한 생존자. 네모네모해진 이상 강력한 성녀의 힘은 더 이상 쓸 수 없다.";
+        characterDescription[6] = "백야 클랜 최고의 문제아. 불꽃의 창 데이브레이커를 되찾기 위해서는 도망쳐야 한다!";
+        characterDescription[7] = "황금룡의 힘을 가진 괴이 소년. 물론 네모난 세상 안에선 그런 거 없이 평등하게 무능하다.";
+        characterDescription[8] = "프리메이슨의 일원인 영국 신사. 대영제국의 부활을 위해서라면 얼마든지 도망칠 수 있다.";
+        characterDescription[9] = "카드 마술이 주특기인 마술사. 친구에게 넘겨받은 희망을 품고 멀리 달려나가기로 한다.";
+        characterDescription[10] = "마검 홍련을 가진 검객 소녀. 그녀의 예술을 계속하기 위해서는 이곳에서 탈출해야 한다!";
+        characterDescription[11] = "별의 영혼을 부르는 사막 왕자. 소꿉친구와 재회하기 전에 이런 곳에서 죽을 수는 없다.";
+        characterDescription[12] = "아가씨가 되고싶은 격투가 소녀. 어째서인지 미래의 힘을 빌려오는 능력이 발동하지 않는다!";
+        characterDescription[13] = "사지에 강철을 단 연금술사. 여기선 연성을 사용할 수 없다. 국가연성진의 끝까지 도망쳐라!";
 
     }
 }
